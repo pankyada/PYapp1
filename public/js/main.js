248c3990a62a36929a556ae6036e1b982f0bde64
pankyada <30976401+pankyada@users.noreply.github.com>
2017-08-15 23:32:31 +0530

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,47 @@
 //  console.log(data);
 //});
 
+function login() {
+
+  var user = document.getElementById('username');
+  var password = document.getElementById('password');
+  var loggedin = false;
+
+  $.ajax({
+    url: 'json/actor.json',
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function(data) {
+      $(data.users).each(function(index, value) {
+        if ((value.userName == user.value) && (value.password == password.value)) {
+          console.log(value.userName);
+          console.log(user.value);
+          console.log(value.password);
+          console.log(password.value);
+        }
+      });
+    }
+  });
 
 
-$.ajax({
-  url:'json/actor.json',
-  dataType:'json',
-  type:'get',
-  cache:false,
-  success: function(data) {
-    $(data.users).each(function(index,value){
-      console.log(value.userName);
-    });
+  alert(loggedin);
+  if(loggedin == true) {
+    alert("You are loggged in Successfully as "+ user.value);
+  } else {
+    alert("Login Failed, check your Username/Password");
   }
-});
+}
+  /*if (user.value == coruser) {
+
+    if (password.value == corpass) {
+      window.alert("you are logged in as " + user.value);
+    } else {
+      window.alert("Incorrect userName or password");
+    }
+  } else {
+    window.alert("Incorrect userName or password");
+
+  }
+}
+*/
